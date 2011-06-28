21f2490c4c62de9878073b14d74a31484c915bee
William Hosbein <william.hosbein@gmail.com>
2011-06-28 14:52:37 -0500

    Added gen_database.py
    Decided that this project will cosist of (for now) two main files: one
    for generating the db and one for querying and getting information
    out of the generated db.
    
    gen_database.py contains the functions to build the database and its
    tables. Currently it generates the file table which contains info on all
    the files.
    
    filetest.py will continue to be my messing around file to get the
    functions ready for gen_database.py.

---
+++ gen_database.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+Methods to create and fill the database containing information about the files.
+"""
+
+import sys, os, string, sqlite3
+from argparse import ArgumentParser
+
+def gen_info(db_name, local_path, c, conn):
+    """
+    Generates the information for the database.
+    """
+
+    file_types = set()
+    urls = set(['.php', '.htm', '.html'])
+
+    for root, dirs, files in os.walk(local_path):
+        for name in files:
+            filename = os.path.join(root, name) # full filename
+            # name is individual file name
+            ext = os.path.splitext(filename)[1]
+            file_types.add(ext)
+            # print extension
+            path = string.replace(root, local_path, '')
+            depth = 0
+            if path != '':
+                depth = path.count('/') + 1
+            size = os.path.getsize(filename)
+            if ext in urls:
+                file_type = 'url'
+            else:
+                file_type = 'file'
+
+            big_insert = ''
+            #sql = 'insert into file (name, depth, path, size, type, ext) ' \
+            big_insert += 'insert into file (name, depth, path, size, type, ext) ' \
+                  'values (\'%s\', %i, \'%s\', %i, \'%s\', \'%s\');' \
+                  % (name, depth, path, size, file_type, ext)
+            #c.execute(sql)
+            c.execute(big_insert)
+            #conn.commit()
+            #print big_insert
+
+            #print 'File name: %s | depth: %s | path: %s | size: %d | ' \
+            #    'type: %s | ext: %s' \
+            #    % (name, depth, path, size, file_type, ext)
+
+    c.execute(big_insert)
+    conn.commit()
+
+def check_for_database(db_name):
+    """
+    Checks for the database and its required tables and creates them if they
+    don't exist.
+
+    Assumes the database is called [db_name].sqlite
+    """
+
+    db_name = db_name + '.sqlite'
+
+    if not os.path.exists(db_name):
+        print "Creating database %s" % db_name
+        file = open(db_name, 'w')
+        file.close()
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    sql = 'create table if not exists file (f_id INTEGER PRIMARY KEY, \
+          name TEXT, depth INT, path TEXT, live BOOLEAN DEFAULT 0, size INT, \
+          type TEXT, ext TEXT)'
+    c.execute(sql)
+    conn.commit()
+
+    return c, conn
+
+def main(argv=None):
+    parser = ArgumentParser(description='Database creator')
+
+    parser.add_argument('-n', '--name', action='store', dest='name',
+                        required=True,
+                        help='The name of the database')
+    parser.add_argument('-p', '--path', action='store', dest='path',
+                        required=True,
+                        help='The path to the local files')
+    args = parser.parse_args()
+
+    c, conn = check_for_database(args.name)
+
+    gen_info(args.name, args.path, c, conn)
+
+if __name__ == "__main__":
+    main()
