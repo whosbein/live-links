23ee0359ac036df02dd7f6230edf49910db968e5
William Hosbein <william.hosbein@gmail.com>
2011-07-06 14:21:43 -0500

    filetest.py now a proper module
    Created a main method and put the correct stuff inside it so I can treat
    filetest.py as a module for testing with the python command line thing.

--- filetest.py
+++ filetest.py
@@ -8,18 +8,6 @@ import lxml.html as l, urllib2
 from urlparse import urljoin
 import re
 
-location = sys.argv[1]
-
-health_url = 'http://www.austincc.edu/health'
-#static = 'ota/' #usually just health/
-static = '' #usually just health/
-#search_url = 'http://www.austincc.edu/health/ota/'
-search_url = 'http://www.austincc.edu/health'
-
-#base_url needs to be acc_url + health_path + current directory in os.walk
-
-file_types = set()
-urls = set(['.php', '.htm', '.html'])
 
 def gen_url(in_url, current_file):
     url = ''
@@ -38,38 +26,52 @@ def fix_implied_index(in_url):
     return in_url
 
 
-for root, dirs, files in os.walk(location):
-    for name in files:
-        filename = os.path.join(root, name) # full filename
-        # name is individual file name
-        ext = os.path.splitext(filename)[1]
-        #print 'root: %s ext: %s' % (root, ext)
-        if ext in urls:
-            file = 'file:' + filename
-            print 'Links in %s' % file
-            doc = l.parse(urllib2.urlopen(file))
-            for link in doc.iter('a'):
-                #current_file = 
-                #print 'current_dir: %s' % current_dir
-                #link.make_links_absolute('http://www.austincc.edu/health/rnsg/guh.php')
-                #print '%s: %s' % (link.text_content(), link.get('href'))
-                #print link
-                #link_target = link.find('href')
-                #print link_target
-                try:
-                    link_target = link.get('href')
-                    # if the link is not absolute: documents/cal.pdf etc
-                    if 'http://' not in link_target:
-                        link_target = gen_url(link_target, filename)
-                    #print '%s: %s' % (link.text_content(), link_target)
-                    #print '%s' % link.cssselect('href')
-                    if search_url in link_target \
-                            and '#' not in link_target \
-                            and '$' not in link_target:
-                        link_target = fix_implied_index(link_target)
-                        print '%s' % link_target
-                except TypeError:
-                    pass
-            print '---------------------'
+def main():
+    location = sys.argv[1]
 
+    health_url = 'http://www.austincc.edu/health'
+    #static = 'ota/' #usually just health/
+    static = '' #usually just health/
+    #search_url = 'http://www.austincc.edu/health/ota/'
+    search_url = 'http://www.austincc.edu/health'
 
+    #base_url needs to be acc_url + health_path + current directory in os.walk
+
+    file_types = set()
+    urls = set(['.php', '.htm', '.html'])
+
+    for root, dirs, files in os.walk(location):
+        for name in files:
+            filename = os.path.join(root, name) # full filename
+            # name is individual file name
+            ext = os.path.splitext(filename)[1]
+            #print 'root: %s ext: %s' % (root, ext)
+            if ext in urls:
+                file = 'file:' + filename
+                print 'Links in %s' % file
+                doc = l.parse(urllib2.urlopen(file))
+                for link in doc.iter('a'):
+                    #current_file = 
+                    #print 'current_dir: %s' % current_dir
+                    #print '%s: %s' % (link.text_content(), link.get('href'))
+                    #print link
+                    #link_target = link.find('href')
+                    #print link_target
+                    try:
+                        link_target = link.get('href')
+                        # if the link is not absolute: documents/cal.pdf etc
+                        if 'http://' not in link_target:
+                            link_target = gen_url(link_target, filename)
+                        #print '%s: %s' % (link.text_content(), link_target)
+                        #print '%s' % link.cssselect('href')
+                        if search_url in link_target \
+                                and '#' not in link_target \
+                                and '$' not in link_target:
+                            link_target = fix_implied_index(link_target)
+                            print '%s' % link_target
+                    except TypeError:
+                        pass
+                print '---------------------'
+
+if __name__ == "__main__":
+    main()
